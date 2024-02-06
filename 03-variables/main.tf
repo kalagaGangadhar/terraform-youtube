3f06284b5640c5eb0b82f0ed52bd5918e95950cd
Gangadhar <gangadharkalaga@gmail.com>
2024-02-07 01:15:36 +0530

    terraform

---
+++ 03-variables/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+resource "aws_instance" "instance-1" {
+  ami                    = "${var.ami}"
+  instance_type          = "${var.instance_type}"
+  vpc_security_group_ids = [ aws_default_security_group.SG-1.id ]
+  tags = {
+    Name="${var.instance_name}"
+  }
+  subnet_id = aws_subnet.subnet-1.id
+
+}
+
+resource "aws_default_security_group" "SG-1" {
+  vpc_id = aws_vpc.vpc-1.id
+}
+
+resource "aws_vpc" "vpc-1" {
+  cidr_block = "${var.vpc_cidr_block}"
+  tags = {
+    Name="${var.vpc_name}"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.vpc-1.id
+  cidr_block = "${var.subnet_cidr_block}"
+  tags = {
+    Name="${var.subnet_name}"
+  }
+}
---
+++ 03-variables/terraform.tfvars
@@ -0,0 +1,8 @@
+region="us-east-1"
+ami="ami-0f3c7d07486cad139"
+instance_type="t2.micro"
+instance_name="ganga_ec2"
+vpc_cidr_block="10.0.0.0/16"
+vpc_name="ganga_vpc"
+subnet_cidr_block="10.0.1.0/24"
+subnet_name="ganga_subnet_1"
