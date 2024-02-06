5e4d437fa91d628da2b48dd7b12a6fd7023607e5
Gangadhar <gangadharkalaga@gmail.com>
2024-02-06 22:54:33 +0530

    terraform

---
+++ 02-resources/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "first" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t2.micro"
+  tags = {
+    name="one"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    name="first"
+  }
+}
