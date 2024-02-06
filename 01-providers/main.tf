e75d1788db4eb67c3b50032bdab828feec631c99
Gangadhar <gangadharkalaga@gmail.com>
2024-02-06 21:11:00 +0530

    terraform

--- 01-providers/main.tf
+++ 01-providers/main.tf
@@ -10,5 +10,5 @@ resource "aws_instance" "example" {
   }
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
-  security_groups = [ "sg-08db871bebc41e267" ]
+  vpc_security_group_ids = [ "sg-08db871bebc41e267" ]
 }
