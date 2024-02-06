fb714ede119b2b02b630deedcf3f092dc98f58d8
Gangadhar <gangadharkalaga@gmail.com>
2024-02-06 23:21:29 +0530

    terraform

--- 02-resources/main.tf
+++ 02-resources/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "first" {
+resource "aws_instance" "instance-1" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
   tags = {
@@ -10,9 +10,14 @@ resource "aws_instance" "first" {
   }
 }
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "vpc-1" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name="first"
   }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id = aws_vpc.vpc-1.id
+  cidr_block = "10.0.1.0/24"
 }
