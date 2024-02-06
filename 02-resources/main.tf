d81666c8e326aa99d843fb97247c933cc7b2402e
Gangadhar <gangadharkalaga@gmail.com>
2024-02-06 23:06:04 +0530

    terraform

--- 02-resources/main.tf
+++ 02-resources/main.tf
@@ -6,13 +6,13 @@ resource "aws_instance" "first" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
   tags = {
-    name="one"
+    Name="one"
   }
 }
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    name="first"
+    Name="first"
   }
 }
