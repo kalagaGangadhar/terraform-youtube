c8f504b8d6dc132bcbd25b1ddfbe3f9cefb2550b
Gangadhar <gangadharkalaga@gmail.com>
2024-02-06 23:56:10 +0530

    terraform

--- 02-resources/main.tf
+++ 02-resources/main.tf
@@ -5,8 +5,7 @@ provider "aws" {
 resource "aws_instance" "instance-1" {
   ami                    = "ami-0f3c7d07486cad139"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [ "sg-08db871bebc41e267" ]
-#  vpc_security_group_ids = aws_vpc.vpc-1.default_security_group_id
+  vpc_security_group_ids = aws_vpc.vpc-1.default_security_group_id
   tags = {
     Name="one"
   }
