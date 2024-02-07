84ee415904674eb6b185af4150f8b49bb9648344
Gangadhar <gangadharkalaga@gmail.com>
2024-02-08 00:20:49 +0530

    terraform

---
+++ 07-local variable/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "instance-1" {
+  ami                    = "ami-0f3c7d07486cad139"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [ local.security ]
+  tags = {
+    Name="one"
+  }
+  subnet_id = aws_subnet.subnet-1.id
+
+}
+
+
+locals {
+  security=aws_default_security_group.SG-1.id
+}
+resource "aws_default_security_group" "SG-1" {
+  vpc_id = aws_vpc.vpc-1.id
+}
+
+resource "aws_vpc" "vpc-1" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name="first"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.vpc-1.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name="subnet-1"
+  }
+}
