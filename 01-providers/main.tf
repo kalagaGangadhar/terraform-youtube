fae2cb4f0d1244cddf19ea80898766fb9e5663ad
Gangadhar <gangadharkalaga@gmail.com>
2024-02-07 21:16:39 +0530

    terraform

--- 01-providers/main.tf
+++ 01-providers/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
+# Create a instance
 resource "aws_instance" "example" {
   tags = {
     Name = "main"
@@ -11,4 +11,11 @@ resource "aws_instance" "example" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
   vpc_security_group_ids = [ "sg-08db871bebc41e267" ]
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "my-tf-test-bucket"
+  tags = {
+    Name        = "My_ganga_bucket"
+  }
 }
