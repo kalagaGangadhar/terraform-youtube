6e3cb14484e3635cfb1bba60b78dc3b9e0de3d57
Gangadhar <gangadharkalaga@gmail.com>
2024-02-07 21:42:58 +0530

    terraform

---
+++ 04-remote-statefile/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "my-groboshop-bucket"
+    key            = "04-remote-statefile/terraform.tfstate"
+#    region         = "us-east-1"
+  }
+}
+
+# Create a instance
+resource "aws_instance" "example" {
+  tags = {
+    Name = "main"
+  }
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-08db871bebc41e267" ]
+}
+
