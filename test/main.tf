494cab4855b96b11365c8224c7cbb50de97beaf0
Gangadhar <gangadharkalaga@gmail.com>
2024-05-03 16:08:09 +0530

    learn terraform

--- test/main.tf
+++ test/main.tf
@@ -2,16 +2,33 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create a instance
-  resource "aws_instance" "example" {
+resource "aws_instance" "instance" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
   tags = {
-    Name = "test"
+    Name = "test-ins"
   }
-  vpc_security_group_ids = [ "sg-08db871bebc41e267" ]
+
+  vpc_security_group_ids = [aws_default_security_group.SG.id]
+  subnet_id = aws_subnet.vpc-sub.id
 }
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "my-groboshop-bucket"
+resource "aws_default_security_group" "SG" {
+  vpc_id = aws_vpc.vpc-1.id
+}
+
+resource "aws_vpc" "vpc-1" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "test-vpc"
+  }
+}
+
+resource "aws_subnet" "vpc-sub" {
+  vpc_id = aws_vpc.vpc-1.id
+  cidr_block = "10.1.0.0/24"
+
+  tags = {
+    Name = "test-subnet"
+  }
 }
