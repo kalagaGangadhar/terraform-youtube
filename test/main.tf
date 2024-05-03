878384cad2430bca599004eeebd9c51ad986569f
Gangadhar <gangadharkalaga@gmail.com>
2024-05-03 21:07:57 +0530

    learn terraform

--- test/main.tf
+++ test/main.tf
@@ -2,46 +2,26 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "instance-1" {
-  ami                    = "ami-0f3c7d07486cad139"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [ aws_default_security_group.SG-1.id ]
-  tags = {
-    Name="one"
-  }
-  subnet_id = aws_subnet.subnet-1.id
 
-}
 
-resource "aws_default_security_group" "SG-1" {
-  vpc_id = aws_vpc.vpc-1.id
-  ingress {
-    protocol  = -1
-    self      = true
-    from_port = 0
-    to_port   = 0
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+terraform {
+  backend "s3" {
+    bucket         = "my-groboshop-bucket"
+    key            = "04-remote-statefile/terraform.tfstate"
+    region         = "us-east-1"
   }
 }
 
-resource "aws_vpc" "vpc-1" {
-  cidr_block = "10.0.0.0/16"
+# Create a instance
+resource "aws_instance" "example" {
   tags = {
-    Name="first"
+    Name = "main"
   }
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-08db871bebc41e267" ]
 }
 
-resource "aws_subnet" "subnet-1" {
-  vpc_id     = aws_vpc.vpc-1.id
-  cidr_block = "10.0.1.0/24"
-  map_public_ip_on_launch = "true"
-  tags = {
-    Name="subnet-1"
-  }
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "my-groboshop-bucket-1"
 }
