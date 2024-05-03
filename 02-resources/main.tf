dd364d16ea2abd299b01a53817578f122b288b7c
Gangadhar <gangadharkalaga@gmail.com>
2024-05-03 17:36:36 +0530

    learn terraform

--- 02-resources/main.tf
+++ 02-resources/main.tf
@@ -27,6 +27,7 @@ resource "aws_vpc" "vpc-1" {
 resource "aws_subnet" "subnet-1" {
   vpc_id     = aws_vpc.vpc-1.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
   tags = {
     Name="subnet-1"
   }
